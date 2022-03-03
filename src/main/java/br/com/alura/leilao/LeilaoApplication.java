bd7842eb59e909f774cbf693be6df53e09990ff7
felipebevilacqua <felipe.bevilacqua@maps.com.br>
2022-03-03 14:50:02 -0300

    Refatorando

--- src/main/java/br/com/alura/leilao/LeilaoApplication.java
+++ src/main/java/br/com/alura/leilao/LeilaoApplication.java
@@ -2,15 +2,12 @@ package br.com.alura.leilao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class LeilaoApplication {
 
 	public static void main(String[] args) {
 
-		System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "prod");
 		SpringApplication.run(LeilaoApplication.class, args);
 	}
-
 }
